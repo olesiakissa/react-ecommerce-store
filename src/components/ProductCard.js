1464d7875d68efd341d325eeb9665c06f20d9efc
Olesia <okissaw@gmail.com>
2022-02-16 14:37:20 +0200

    Add styling for the 'out of stock' items

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,14 +6,16 @@ export default class ProductCard extends React.Component {
     const filteredPrice = this.props.prices.find(
       price => price.currency.symbol === this.props.currentCurrency).amount;
     return (
-      <div className='product-card'>
-      <img src={this.props.gallery[0]} alt={this.props.name} />
+      <div className={`product-card ${!this.props.inStock && 'out-of-stock'}`}>
+      <img src={this.props.gallery[0]} className='product-thumbnail' alt={this.props.name} />
+      <div className='out-of-stock-text' style={{display: `${!this.props.inStock ? 'block' : 'none'}`}}>OUT OF STOCK</div>
       <h2 className='product-name'>{this.props.name}</h2>
       <div className='flex product-cta'>
         <p className='product-price'>{`${this.props.currentCurrency}${filteredPrice}`}</p>
         <button aria-label='Add to cart'
+                disabled={this.props.inStock ? false : true}
                 className='product-card-btn-addToCart'
-                onClick={(e)=>this.props.addToCart(e, this.props)}>
+                onClick={()=>this.props.addToCart(this.props)}>
         </button>
       </div>
       </div>
