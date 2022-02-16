567a0843a256e0a6bd7e8ebdbcb2f0da75f924be
Olesia <okissaw@gmail.com>
2022-02-16 18:21:33 +0200

    Configure routes for the index, PDP and cart pages

--- src/components/ProductDescriptionPage.js
+++ src/components/ProductDescriptionPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class App extends React.Component {
+export default class ProductDescriptionPage extends React.Component {
   render() {
     return (
       <></>
